6a743579f3e7d93ab4a308eb165b68fce3a07668
Brandon-Cameron <c00260441@setu.ie>
2024-09-18 13:53:24 +0100

    Tiles filling empty grid

--- Project1/Project1/main.cpp
+++ Project1/Project1/main.cpp
@@ -14,6 +14,53 @@
 
 #include <SFML/Graphics.hpp>
 #include <iostream>
+#include <vector>
+
+class Tile
+{
+public:
+
+	void create(int t_type, int t_x, int t_y)
+	{
+		m_type = t_type;
+
+		m_shape.setSize(sf::Vector2f(100, 100));
+		if (t_type == 0)
+		{
+			m_shape.setFillColor(sf::Color::Transparent);
+		}
+		if (t_type == 1)
+		{
+			m_shape.setFillColor(sf::Color::Magenta);
+		}
+		if (t_type == 2)
+		{
+			m_shape.setFillColor(sf::Color::Cyan);
+		}
+		if (t_type == 3)
+		{
+			m_shape.setFillColor(sf::Color::Red);
+		}
+		if (t_type == 4)
+		{
+			m_shape.setFillColor(sf::Color::Green);
+		}
+		if (t_type == 5)
+		{
+			m_shape.setFillColor(sf::Color::Yellow);
+		}
+		m_shape.setPosition(t_x, t_y);
+	}
+
+	int getType()
+	{
+		return m_type;
+	}
+
+	int m_type;
+	sf::RectangleShape m_shape;
+
+};
 
 int main()
 {
@@ -21,7 +68,7 @@ int main()
 
 	std::srand(std::time(nullptr));
 
-	sf::RectangleShape player[81];
+	Tile tiles[81];
 
 
 	int gridData[] =
@@ -36,17 +83,18 @@ int main()
 		0, 0, 0, 0, 0, 0, 0, 0,
 	};
 
-	for (int i = 0; i < 64; i++)
+	/*for (int i = 0; i < 64; i++)
 	{
 		gridData[i] = (std::rand() % 5) + 1;
-	};
+	};*/
 
 	int xOffset = 100;
 	int yOffset = 100;
 	int tileNo = 0;
+	int dropCount = 0;
 
 
-	sf::Time timePerFrame = sf::seconds(1.0f / 60.0f);
+	sf::Time timePerFrame = sf::seconds(1.0f / 5.0f);
 	sf::Time timeSinceLastUpdate = sf::Time::Zero;
 	sf::Clock clockForFrameRate;
 
@@ -63,52 +111,98 @@ int main()
 		timeSinceLastUpdate += clockForFrameRate.restart();
 
 		if (timeSinceLastUpdate > timePerFrame)
-		{
-
-			for (int i = 0; i < 8; i++)
+		{	// CODE FOR UPDATE THE BOARD, CREATING AND DROPPING TILES
+			if (dropCount == 10)
 			{
-				xOffset = 100;
 				for (int i = 0; i < 8; i++)
 				{
-					if (gridData[tileNo] == 1)
+					if (gridData[i] == 0)
 					{
-						player[tileNo].setSize(sf::Vector2f(100, 100));
-						player[tileNo].setFillColor(sf::Color::Magenta);
-						player[tileNo].setPosition(xOffset, yOffset);
+						tiles[i].create((std::rand() % 5) + 1, xOffset, yOffset);
+						gridData[i] = tiles[i].m_type;
+
+						xOffset += 100;
+
 					}
-					if (gridData[tileNo] == 2)
-					{
-						player[tileNo].setSize(sf::Vector2f(100, 100));
-						player[tileNo].setFillColor(sf::Color::Cyan);
-						player[tileNo].setPosition(xOffset, yOffset);
-					}
-					if (gridData[tileNo] == 3)
-					{
-						player[tileNo].setSize(sf::Vector2f(100, 100));
-						player[tileNo].setFillColor(sf::Color::Red);
-						player[tileNo].setPosition(xOffset, yOffset);
-					}
-					if (gridData[tileNo] == 4)
-					{
-						player[tileNo].setSize(sf::Vector2f(100, 100));
-						player[tileNo].setFillColor(sf::Color::Green);
-						player[tileNo].setPosition(xOffset, yOffset);
-					}
-					if (gridData[tileNo] == 5)
-					{
-						player[tileNo].setSize(sf::Vector2f(100, 100));
-						player[tileNo].setFillColor(sf::Color::Yellow);
-						player[tileNo].setPosition(xOffset, yOffset);
-					}
-					tileNo++;
-					xOffset += 100;
 				}
-				yOffset += 100;
+				for (int row = 0; row < 7; row++)
+				{
+					xOffset = 100;
+					for (int col = 0; col < 8; col++)
+					{
+						if (gridData[(col + 8 * row) + 8] == 0)
+						{
+							//tiles[i + 8].create(tiles[i].m_type, xOffset, yOffset);
+							gridData[(col + 8 * row) + 8] = gridData[(col + 8 * row)];
+							gridData[col + 8 * row] = 0;
+
+							//tiles[i].m_type = 0;
+
+							xOffset += 100;
+						}
+					}
+					yOffset += 100;
+				}
+				dropCount = 0;
+
+
+				yOffset = 100;
+				std::cout << "********************" << "\n";
+				for (int row = 0; row < 8; row++)
+				{
+					for (int col = 0; col < 8; col++)
+					{
+						std::cout << gridData[col + (row * 8)];
+					}
+					std::cout << "\n";
+
+				}
+				std::cout << "********************" << "\n";
+
+
+				for (int i = 0; i < 8; i++)
+				{
+					xOffset = 100;
+					for (int i = 0; i < 8; i++)
+					{
+						tiles[tileNo].create(gridData[tileNo], xOffset, yOffset);
+
+						tileNo++;
+						xOffset += 100;
+					}
+					yOffset += 100;
+				}
+
+
+				tileNo = 0;
+				std::cout << tiles[30].m_type << "\n";
+
+				// CHECK AND REMOVE MATCHED TILES
+
+				for (int row = 0; row < 8; row++)
+				{
+					for (int col = 0; col < 8; col++)
+					{
+						int index = col + 8 * row;
+
+						if(gridData[index] == gridData[index + 8] && gridData[index] == gridData[index - 8])
+						{
+							gridData[col + 8 * row] = 0;
+							gridData[(col + 8 * row) + 8] = 0;
+							gridData[(col + 8 * row) - 8] = 0;
+							//std::cout << "Vertical Clear: " << tiles[tileNo - 8].m_type << tiles[tileNo].m_type << tiles[tileNo + 8].m_type << "\n";
+							
+						}
+
+						tileNo++;
+					}
+				}
 			}
 
 			tileNo = 0;
 			xOffset = 100;
 			yOffset = 100;
+			dropCount++;
 
 
 
@@ -117,7 +211,7 @@ int main()
 			for (int i = 0; i < 64; i++)
 			{
 				//player[i].setPosition(player[i].getPosition().x + 1, player[i].getPosition().y);
-				window.draw(player[i]);
+				window.draw(tiles[i].m_shape);
 			};
 
 			window.display();
